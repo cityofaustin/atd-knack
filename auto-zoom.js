dcab3db4297cc5284ab095d488da848684fbe402
Mateo Clarke <code@mateoclarke.com>
2019-06-24 16:40:04 -0500

    try script ready

--- auto-zoom.js
+++ auto-zoom.js
@@ -3,6 +3,8 @@ script.src = "https://code.jquery.com/jquery-1.11.0.min.js";
 script.type = "text/javascript";
 document.getElementsByTagName("head")[0].appendChild(script);
 
-$(document).on("knack-view-render.view_2587", function(event, scene) {
-  console.log("auto-zooom script loaded");
+script.ready(function() {
+  $(document).on("knack-view-render.view_2587", function(event, scene) {
+    console.log("auto-zooom script loaded");
+  });
 });
