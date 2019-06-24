4106b1fdccdb742309148d777c2c8b15cea874df
Mateo Clarke <code@mateoclarke.com>
2019-06-24 16:34:37 -0500

    add jquery import

--- auto-zoom.js
+++ auto-zoom.js
@@ -1,3 +1,8 @@
+var script = document.createElement("script");
+script.src = "http://code.jquery.com/jquery-1.11.0.min.js";
+script.type = "text/javascript";
+document.getElementsByTagName("head")[0].appendChild(script);
+
 $(document).on("knack-view-render.view_2587", function(event, scene) {
   console.log("auto-zooom script loaded");
 });
