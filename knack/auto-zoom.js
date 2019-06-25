cc7cdd5d168caaef880eb3fab935b4ae984e9953
Mateo Clarke <code@mateoclarke.com>
2019-06-25 10:35:03 -0500

    add wrapper for jQuery import

--- knack/auto-zoom.js
+++ knack/auto-zoom.js
@@ -1,10 +1,35 @@
-var script = document.createElement("script");
-script.src = "https://code.jquery.com/jquery-1.11.0.min.js";
-script.type = "text/javascript";
-document.getElementsByTagName("head")[0].appendChild(script);
+(function() {
+  var startingTime = new Date().getTime();
+  // Import jQuery into this file from CDN
+  // https://stackoverflow.com/questions/34338411/how-to-import-jquery-using-es6-syntax
+  var script = document.createElement("SCRIPT");
+  script.src =
+    "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+  script.type = "text/javascript";
+  document.getElementsByTagName("head")[0].appendChild(script);
 
-script.ready(function() {
-  $(document).on("knack-view-render.view_2587", function(event, scene) {
-    console.log("autozooom script loaded");
+  // Create polling function for made sure jQuery is loaded and ready...
+  var checkReady = function(callback) {
+    if (window.jQuery) {
+      callback(jQuery);
+    } else {
+      window.setTimeout(function() {
+        checkReady(callback);
+      }, 20);
+    }
+  };
+
+  // Start polling...
+  checkReady(function($) {
+    $(function() {
+      var endingTime = new Date().getTime();
+      var tookTime = endingTime - startingTime;
+      window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+    });
+
+    // Our actual Knack custom code goes here...
+    $(document).on("knack-view-render.view_2587", function(event, scene) {
+      console.log("autozooom script loaded");
+    });
   });
-});
+})();
