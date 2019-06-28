6c6393481fa76e367c730ae49f92e16ce8a48cf0
Mike Dilley <mike.dilley@gmail.com>
2019-06-28 14:27:03 -0500

    Add TODO for assigning record id from breadcrumb

--- knack/iframeMapMessenger.js
+++ knack/iframeMapMessenger.js
@@ -36,71 +36,72 @@
     debugger;
     var $viewSelector = $(myView);
 
+    var recordId = Knack.hash_id;
+
+    // TODO breadcrumb not loaded yet, .load() generates TypeError
+    // $(".kn-crumbtrail").load(function() {
+    //   debugger;
+    //   recordId = $($(".kn-crumbtrail").children()[2])
+    //     .attr("href")
+    //     .split("/")[2]
+    //     .slice(0, -1);
+    // });
+
     // Message for React app API call for sign records
-    var recordId = "";
+    var markerMessage = {
+      message: "SIGNS_API_REQUEST",
+      view: myView.slice(1),
+      scene: "scene_1028",
+      token: Knack.getUserToken(),
+      app_id: Knack.application_id,
+      id: recordId
+    };
 
-    $(".kn-crumbtrail").load(function() {
-      debugger;
-      recordId = $($(".kn-crumbtrail").children()[2])
-        .attr("href")
-        .split("/")[2]
-        .slice(0, -1);
+    // Add React app as iframe
+    $(
+      '<iframe src="https://localhost:9001" frameborder="0" scrolling="yes" id="mapIFrame" \
+    style="width: 100%;height: 523px;"></iframe>'
+    ).appendTo($viewSelector);
 
-      var markerMessage = {
-        message: "SIGNS_API_REQUEST",
-        view: myView.slice(1),
-        scene: "scene_1028",
-        token: Knack.getUserToken(),
-        app_id: Knack.application_id,
-        id: recordId
+    // set up Post Message connection with iframe and parent page
+    var iframe = document.getElementById("mapIFrame").contentWindow;
+
+    function sendMessageToApp(message) {
+      var stringifiedMessage = JSON.stringify(message);
+      console.log("inside API", stringifiedMessage);
+      iframe.postMessage(stringifiedMessage, "*");
+    }
+
+    // listen for response
+    window.addEventListener("message", function(event) {
+      console.log("message received:  " + event.data, event);
+      var data = event.data;
+      if (data.message === "LAT_LON_FIELDS") {
+        var $latLonFields = $("#kn-input-field_3194");
+
+        $latLonFields.find("#latitude").val(data.lat);
+        $latLonFields.find("[name='longitude']").val(data.lng);
+      }
+    });
+
+    $("#mapIFrame").load(function() {
+      sendMessageToApp(markerMessage);
+    });
+
+    // $(function() {
+    // Get the current location from browser.
+    // TODO: we might need to wrap this in try/catch check
+    navigator.geolocation.getCurrentPosition(function(position) {
+      // create message object for React App
+      const geolocationMessage = {
+        message: "KNACK_GEOLOCATION",
+        lat: position.coords.latitude,
+        lon: position.coords.longitude
       };
 
-      // Add React app as iframe
-      $(
-        '<iframe src="https://localhost:9001" frameborder="0" scrolling="yes" id="mapIFrame" \
-    style="width: 100%;height: 523px;"></iframe>'
-      ).appendTo($viewSelector);
-
-      // set up Post Message connection with iframe and parent page
-      var iframe = document.getElementById("mapIFrame").contentWindow;
-
-      function sendMessageToApp(message) {
-        var stringifiedMessage = JSON.stringify(message);
-        console.log("inside API", stringifiedMessage);
-        iframe.postMessage(stringifiedMessage, "*");
-      }
-
-      // listen for response
-      window.addEventListener("message", function(event) {
-        console.log("message received:  " + event.data, event);
-        var data = event.data;
-        if (data.message === "LAT_LON_FIELDS") {
-          var $latLonFields = $("#kn-input-field_3194");
-
-          $latLonFields.find("#latitude").val(data.lat);
-          $latLonFields.find("[name='longitude']").val(data.lng);
-        }
-      });
-
-      $("#mapIFrame").load(function() {
-        sendMessageToApp(markerMessage);
-      });
-
-      // $(function() {
-      // Get the current location from browser.
-      // TODO: we might need to wrap this in try/catch check
-      navigator.geolocation.getCurrentPosition(function(position) {
-        // create message object for React App
-        const geolocationMessage = {
-          message: "KNACK_GEOLOCATION",
-          lat: position.coords.latitude,
-          lon: position.coords.longitude
-        };
-
-        // envoke message once the iframe is loaded
-        $("#mapIFrame").on("load", function() {
-          AutozoomSendMessageToApp(geolocationMessage);
-        });
+      // envoke message once the iframe is loaded
+      $("#mapIFrame").on("load", function() {
+        AutozoomSendMessageToApp(geolocationMessage);
       });
     });
     // });
