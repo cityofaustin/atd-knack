e6083d3fb9807c904d032468f8fa6d31357745bb
Mateo Clarke <code@mateoclarke.com>
2019-07-10 19:02:49 -0500

    update which external js file we use

--- knack/index.js
+++ knack/index.js
@@ -354,7 +354,7 @@ $(document).on("knack-view-render.view_2465", function(event, page) {
 
 function loadIframeMapMessenger(viewId) {
   var url =
-    "https://dnb4pix4gcpf6.cloudfront.net/atd-knack-signs-markings/user_testing/iframeMapMessenger.js";
+    "https://dnb4pix4gcpf6.cloudfront.net/atd-knack-signs-markings/88_update_view_ids/iframeMapMessenger.js";
   $.getScript(url, function(data, textStatus, jqxhr) {
     console.log(data); // Data returned
     console.log(textStatus); // Success
