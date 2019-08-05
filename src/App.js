550e9a723a177af5b54e18844417623ae034c9ea
tcwcodes <35410637+tcwcodes@users.noreply.github.com>
2019-08-05 14:02:36 -0500

    js-calc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import "./App.css";
 import SelectLocation from "./SelectLocation";
+import Calcs from "./calcs";
 require("dotenv").config();
 
 function App() {
   return (
-    <div className="App">
-      <SelectLocation />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/"
+            render={() =>
+              <div className="App">
+                <SelectLocation />
+              </div>
+            }
+          />
+          <Route exact path ="/calcs"
+            render={() =>
+              <Calcs />
+            }
+          />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
