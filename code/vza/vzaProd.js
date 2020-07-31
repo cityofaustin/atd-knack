09f225794698ae231aa7b9e72c47cf89195ab58a
Mike Dilley <mike.dilley@gmail.com>
2020-07-31 11:47:19 -0500

    Get officer_assignment records

--- code/vza/vzaProd.js
+++ code/vza/vzaProd.js
@@ -1,3 +1,38 @@
 $(document).on("knack-view-render.any", function (event, view, data) {
   $("a.kn-view-asset").html("Attachment");
 });
+
+$(document).on("knack-view-render.view_466", function (event, view, data) {
+  // Define VZA Knack app ID
+  var knackAppId = "5f2440f7ef8de9001620d7e2";
+
+  // Define scene and view of officer_assignments table API view
+  var tableScene = "scene_236";
+  var tableView = "view_484";
+
+  var url =
+    "https://api.knack.com/v1/pages/" +
+    tableScene +
+    "/views/" +
+    tableView +
+    "/records/";
+
+  // Get user auth for get request (API view is private) and set req headers
+  var user = Knack.getUserToken();
+
+  var headers = {
+    "X-Knack-Application-ID": knackAppId,
+    Authorization: user,
+    "content-type": "application/json"
+  };
+
+  // Request officer_assignment records
+  $.ajax({
+    url: url,
+    type: "GET",
+    headers: headers,
+    success: function (res) {
+      console.log(res);
+    }
+  });
+});
