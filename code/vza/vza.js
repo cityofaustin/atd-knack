ca23dcdc469c7a286f0a36405b3225ffe600b5f2
Mike Dilley <mike.dilley@gmail.com>
2020-08-03 11:49:47 -0500

    Break table components up to build shifts and append to table body

--- code/vza/vza.js
+++ code/vza/vza.js
@@ -62,7 +62,7 @@ $(document).on("knack-view-render.view_466", function (event, view, data) {
       appendShiftTable(groupedAssignments);
       console.log(groupedAssignments);
 
-      // Remove spinner if no checkboxes selected
+      // Remove spinner
       $("#assignment-spinner").remove();
     }
   });
@@ -95,7 +95,6 @@ $(document).on("knack-view-render.view_466", function (event, view, data) {
   function appendShiftTable(records) {
     // $("#view_466 > div.view-header").append(JSON.stringify(records));
     var currentPage = 1;
-    var record = Object.values(records)[0][0];
 
     var recordsTotalsDiv = `
       <div class="level" style="margin-bottom: .75em;">
@@ -132,94 +131,111 @@ $(document).on("knack-view-render.view_466", function (event, view, data) {
         </div>
       </div>`;
 
-    var recordsTable = `<div class="kn-table-wrapper">
-    <table class="kn-table kn-table-table is-bordered is-striped">
-      <thead>
-        <tr>
-          <th class="field_139">
-            <span class="table-fixed-label">
-              <a
-                href="#field_139|asc"
-                address="true"
-                class="kn-sort level is-compact"
-              >
-                <span>Time</span>
-              </a>
-            </span>
-          </th>
-          <th class="field_637">
-            <span class="table-fixed-label">
-              <a
-                href="#field_656-field_637|asc"
-                address="true"
-                class="kn-sort level is-compact"
-              >
-                <span>Sector</span>
-              </a>
-            </span>
-          </th>
-          <th class="field_624">
-            <span class="table-fixed-label">
-              <a
-                href="#field_656-field_624|asc"
-                address="true"
-                class="kn-sort level is-compact"
-              >
-                <span>Location</span>
-              </a>
-            </span>
-          </th>
-          <th class="kn-table-action-link">
-            <span class="table-fixed-label">Sign Up</span>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="kn-table-group kn-group-level-1">
-          <td style="" colspan="4">${record.field_724}</td>
-        </tr>
-        <tr id="5f2441103dd0c106c27183ad">
-          <td
-            class="field_139"
-            style="background-color: rgb(82, 157, 198); padding-left: 20px;"
-            data-column-index="1"
-            data-field-key="field_139"
-          >
-            <span class="col-1">
-              ${record.field_139}
-            </span>
-          </td>
-          <td class="field_637" data-column-index="2" data-field-key="field_637">
-            <span class="col-2">
-              <span id="5f24412f3dd0c106c2719524">${
-                record.field_656_raw[0].identifier.split(" - ")[0]
-              }</span>
-            </span>
-          </td>
-          <td class="field_624" data-column-index="3" data-field-key="field_624">
-            <span class="col-3">
-              <span id="5f24412f3dd0c106c2719524"
-                >${record.field_656_raw[0].identifier.split(" - ")[2]}</span
-              >
-            </span>
-          </td>
-          <td class="kn-table-link" style="text-align: center;">
-            <span class="col-4">
-              <a
-                class="kn-link kn-link-page kn-action-link"
-                href="javascript:void(0);"
-                data-action-rule-index="0"
-                style="text-align: center;"
-                ><i class="fa fa-user-plus"></i
-              ></a>
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  `;
+    var recordsTable = `
+    <div class="kn-table-wrapper">
+      <table class="kn-table kn-table-table is-bordered is-striped">
+        <thead>
+          <tr>
+            <th class="field_139">
+              <span class="table-fixed-label">
+                <a
+                  href="#field_139|asc"
+                  address="true"
+                  class="kn-sort level is-compact"
+                >
+                  <span>Time</span>
+                </a>
+              </span>
+            </th>
+            <th class="field_637">
+              <span class="table-fixed-label">
+                <a
+                  href="#field_656-field_637|asc"
+                  address="true"
+                  class="kn-sort level is-compact"
+                >
+                  <span>Sector</span>
+                </a>
+              </span>
+            </th>
+            <th class="field_624">
+              <span class="table-fixed-label">
+                <a
+                  href="#field_656-field_624|asc"
+                  address="true"
+                  class="kn-sort level is-compact"
+                >
+                  <span>Location</span>
+                </a>
+              </span>
+            </th>
+            <th class="kn-table-action-link">
+              <span class="table-fixed-label">Sign Up</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody id="shift-table-body">
+        </tbody>
+      </table>
+    </div>`;
 
-    $("#view_466 > div.view-header").append(recordsTotalsDiv + recordsTable);
+    function buildShiftSection(shiftRecords) {
+      var record = Object.values(shiftRecords)[0][0];
+      console.log(record);
+
+      var shiftTitle = `
+      <tr class="kn-table-group kn-group-level-1">
+        <td style="" colspan="4">${record.field_724}</td>
+      </tr>`;
+
+      var shiftDetails = `
+      <tr>
+        <td
+          class="field_139"
+          style="background-color: rgb(82, 157, 198); padding-left: 20px;"
+          data-column-index="1"
+          data-field-key="field_139"
+        >
+          <span class="col-1">
+            ${record.field_139}
+          </span>
+        </td>
+        <td class="field_637" data-column-index="2" data-field-key="field_637">
+          <span class="col-2">
+            <span id="5f24412f3dd0c106c2719524">${
+              record.field_656_raw[0].identifier.split(" - ")[0]
+            }</span>
+          </span>
+        </td>
+        <td class="field_624" data-column-index="3" data-field-key="field_624">
+          <span class="col-3">
+            <span id="5f24412f3dd0c106c2719524"
+              >${record.field_656_raw[0].identifier.split(" - ")[2]}</span
+            >
+          </span>
+        </td>
+        <td class="kn-table-link" style="text-align: center;">
+          <span class="col-4">
+            <a
+              class="kn-link kn-link-page kn-action-link"
+              href="javascript:void(0);"
+              data-action-rule-index="0"
+              style="text-align: center;"
+              ><i class="fa fa-user-plus"></i
+            ></a>
+          </span>
+        </td>
+      </tr>`;
+
+      return shiftTitle + shiftDetails;
+    }
+
+    // Append table, then append shifts to table body
+    $("#view_466 > div.view-header")
+      .append(recordsTotalsDiv + recordsTable)
+      .ready(function () {
+        var shifts = buildShiftSection(records);
+        $("#shift-table-body").append(shifts);
+      });
   }
 });
