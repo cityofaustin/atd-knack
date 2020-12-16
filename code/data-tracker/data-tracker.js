10088480829c6d05fbd0353f96e84d6d049e919f
John Clary <john.clary@austintexas.gov>
2020-12-15 19:34:43 -0600

    Prevent default behavior on custom refresh button

--- code/data-tracker/data-tracker.js
+++ code/data-tracker/data-tracker.js
@@ -740,7 +740,7 @@ $(document).on("knack-scene-render.scene_1171", function (event, page) {
   });
 });
 
-////// Add "Refresh" button to inventory requests table
+// Add "Refresh" button to inventory requests table
 $(document).on("knack-view-render.view_2698", function (event, page) {
   var button = $(
     "<span style='width: 2em'></span><button id='refresh-view_2698' style='border-radius: .35em !important' class='kn-button is-primary'><i class='fa fa-refresh'></i><span style='width: .5em'></span>Refresh</button>"
@@ -750,7 +750,8 @@ $(document).on("knack-view-render.view_2698", function (event, page) {
     $("#view_2698").find("form.table-keyword-search").find("a")[0]
   );
 
-  $("#refresh-view_2698").click(function () {
+  $("#refresh-view_2698").click(function (e) {
+    e.preventdefault();
     Knack.views["view_2698"].model.fetch();
   });
 });
