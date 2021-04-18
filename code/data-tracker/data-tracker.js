caaed3b5353fcc4c623b10b4cb36348e41d551a0
John Clary <john.clary@austintexas.gov>
2021-04-17 19:49:57 -0500

    Revert inventory requests refresh button deletion

--- code/data-tracker/data-tracker.js
+++ code/data-tracker/data-tracker.js
@@ -618,6 +618,22 @@ $(document).on("knack-scene-render.scene_1171", function (event, page) {
   });
 });
 
+// Add "Refresh" button to inventory requests table
+$(document).on("knack-view-render.view_2698", function (event, page) {
+  var button = $(
+    "<span style='width: 2em'></span><button id='refresh-view_2698' style='border-radius: .35em !important' class='kn-button is-primary'><i class='fa fa-refresh'></i><span style='width: .5em'></span>Refresh</button>"
+  );
+
+  button.insertAfter(
+    $("#view_2698").find("form.table-keyword-search").find("a")[0]
+  );
+
+  $("#refresh-view_2698").click(function (e) {
+    e.preventdefault();
+    Knack.views["view_2698"].model.fetch();
+  });
+});
+
 
 //////////////////////////////////////////////////////
 // Disable editing of task order on work orders   ////
