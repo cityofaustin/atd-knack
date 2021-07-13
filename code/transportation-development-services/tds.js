f31feeae218986a3abbc10694f192767edc746f3
Chia Berry <chia.berry@austintexas.gov>
2021-07-13 11:18:37 -0500

    mobile menu for tia

--- code/transportation-development-services/tds.js
+++ code/transportation-development-services/tds.js
@@ -567,10 +567,11 @@ $(document).on("knack-view-render.view_321", function (event, page) {
 /**** TIA Menu Buttons Navigation ****/
 /*************************************/
 
-function dropdownMenuItem(recordId, route, iconName, linkName, newTab = false) {
+function dropdownMenuItem(recordId, route, iconName, linkName, newTab = false, mobile=false) {
+  const buttonClass = mobile ? "tia-button" : "kn-button";
   if (newTab) {
     return (
-      `<li class="kn-button">\
+      `<li class="${buttonClass}">\
         <a href="#tia-requests/tia-case-details/${recordId}/${route}/${recordId}" target="_blank" and rel="noopener noreferrer">\
           <span class="icon is-small"> \
             <i class="fa ${iconName}" /> \
@@ -580,7 +581,7 @@ function dropdownMenuItem(recordId, route, iconName, linkName, newTab = false) {
       </li>`)
   }
   return (
-    `<li class="kn-button">\
+    `<li class="${buttonClass}">\
       <a href="#tia-requests/tia-case-details/${recordId}/${route}/${recordId}">\
         <span class="icon is-small"> \
           <i class="fa ${iconName}" /> \
@@ -589,6 +590,14 @@ function dropdownMenuItem(recordId, route, iconName, linkName, newTab = false) {
       </a>\
     </li>`)
 }
+
+// Function to toggle "active" and "show-icon" classes for mobile dropdown menu
+// if class is not "active", list is display: none;
+$(document).on('click', '.mobile-dropdown-button', function(event) {
+  const menuList = $(event.target).siblings()[0]
+  $(menuList).toggleClass("active")
+  $(event.target).toggleClass("show-icon")
+});
 
 /* Case Details Page */
 $(document).on('knack-view-render.view_744', function(event, view, record) {
@@ -623,6 +632,39 @@ $(document).on('knack-view-render.view_744', function(event, view, record) {
       </li>\
       ${dropdownMenuItem(recordId, "edit-tia-fee-status-reviewer", "fa-dollar", "Fees")}\
       ${dropdownMenuItem(recordId, "add-tia-communication", "fa-plus-circle", "Communication")}\
+    </ul>\
+  </div>`).appendTo("#view_744")
+
+  // *** Dropdown menu for mobile views ***
+  $(`<div class="mobile-details-dropdown-menu">\
+    <ul id="tia-mobile-menu-list">\
+      <li class="tia-mobile-dropdown-menu">\
+        <span class="tia-button mobile-dropdown-button"> \
+          <i class="fa fa-angle-down tia-dropdown" /> \
+          Case Management Menu\
+        </span>\
+        <ul class="tia-dropdown-menu-list" style="min-width: 152px; margin: .5em;">\
+          ${dropdownMenuItem(recordId, "tia-case-details", "fa-list-alt", "Case Details", false, true)}\
+          ${dropdownMenuItem(recordId, "tia-case-management", "fa-archive", "Scope & Submissions", false, true)}\
+          ${dropdownMenuItem(recordId, "tia-mitigation-details", "fa-file-text-o", "Mitigations", false, true)}\
+          ${dropdownMenuItem(recordId, "memo-builder", "fa-medium", "Memo Builder", false, true)}\
+          ${dropdownMenuItem(recordId, "tia-case-log", "fa-briefcase", "Case Log", false, true)}\
+        </ul>\
+      </li>\
+      <li class="tia-mobile-dropdown-menu">\
+        <span class="tia-button mobile-dropdown-button">\
+          <i class="fa fa-angle-down tia-dropdown" /> \
+          Update Case Details Menu\
+        </span>\
+        <ul class="tia-dropdown-menu-list" style="min-width: 152px; margin: .5em;">\
+          ${dropdownMenuItem(recordId, "edit-tia-case-details", "fa-edit", "Edit Case Details & Notes", false, true)}\
+          ${dropdownMenuItem(recordId, "assign-case-reviewers", "fa-users", "Assign Case Reviewers", false, true)}\
+          ${dropdownMenuItem(recordId, "change-tia-case-status", "fa-retweet", "Approve or Change Case Status", false, true)}\
+          ${dropdownMenuItem(recordId, "connected-cases", "fa-link", "Connect Cases", false, true)}\
+        </ul>\
+      </li>\
+      ${dropdownMenuItem(recordId, "edit-tia-fee-status-reviewer", "fa-dollar", "Fees", false, true)}\
+      ${dropdownMenuItem(recordId, "add-tia-communication", "fa-plus-circle", "Communication", false, true)}\
     </ul>\
   </div>`).appendTo("#view_744")
 })
