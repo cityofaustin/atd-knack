9ac4c675879f7aae7cc5b1031b1dd7daec6e357b
Susanne <31259724+susannegov@users.noreply.github.com>
2024-07-10 14:28:17 -0500

    Update mobility-services.js
    - moved new code to bottom
    - update commenting
    - update print menu function

--- code/mobility-services/mobility-services.js
+++ code/mobility-services/mobility-services.js
@@ -1,21 +1,6 @@
 /***************************************/
-/***** Print Menu Button ************/
-/***************************************/
-function printMenuButton(view_id) {
-  $(document).on('knack-view-render.' + view_id, function(event, view, data) {
-      $('#' + view_id + ' .knMenuLink').click(function(e) {
-        window.print();
-      });
-  });
-}
-
-printMenuButton('view_227'); // Print 3 pages
-printMenuButton('view_228'); // Print 4 pages
-
-/***************************************/
 /**** Input Mask number for SSN ********/
 /***************************************/
-
 $(document).on('knack-view-render.any', function (event, view, data) {
   $('input#field_33').keyup(function(event) { // validates typing
     this.value = this.value.replace(/[-]/g, ''); // replace hyphens with nothing
@@ -35,7 +20,7 @@ $(document).on('knack-view-render.any', function (event, view, data) {
     }
 
   });
-  // Validation
+  /* Validation */
   $("input#field_33").attr('maxlength', 11);
   $("input#field_33").attr('placeholder',"___ __ ____");
 
@@ -44,11 +29,12 @@ $(document).on('knack-view-render.any', function (event, view, data) {
 /********************************************************/
 /********** Show character limit on text type ***********/
 /********************************************************/
+/* Function shows character limit of the text field when filling out the filed in a a form view.*/
 function showCharacterLimit(view_id, field_id, charLimit) {
-  // Function returns list as message string and css dictionary based on the input length and character limit
+  /* Function returns list as message text and css based on the input length and character limit */
   function showMessage(inputField) {
     const fieldLength = inputField.val().length;
-    var inputLength = Math.abs(charLimit - fieldLength); // No negative numbers 
+    var inputLength = Math.abs(charLimit - fieldLength); // No negative numbers
     var fieldText = fieldLength == 0 ? "allowed" : "left";
     var cssField = {"color": "#4a4a4a", "font-weight": "normal"};
     if (fieldLength > charLimit) { // if the length is over the character limit change the CSS and text
@@ -58,14 +44,14 @@ function showCharacterLimit(view_id, field_id, charLimit) {
     return [inputLength + " characters " + fieldText,cssField]; // returns list as [str message, dict css]
   }
 
-  // Shows the message after field input based on character limit and length
+  /* Shows the message after field input based on character limit and length */
   $(document).on("knack-view-render." + view_id, function(event, view, data) {
-    // When first viewing the field input
+    /* When first viewing the field input */
     const formViewFieldID = ".kn-form.kn-view."+ view_id + " form #" + field_id;
     const fieldMessage = showMessage($(formViewFieldID))[0]; 
     $(formViewFieldID).after(`<p class='typed-chars'>${fieldMessage}</p>`);
     
-    // When user is typing in the input field change the text and CSS
+    /* When user is typing in the input field change the text and CSS */
     $(document).ready(function() { 
       $(formViewFieldID).on('input',function(e){
         const $input = $(this);
@@ -76,10 +62,10 @@ function showCharacterLimit(view_id, field_id, charLimit) {
       });
     });
   });
-};
+}; // This closes the showCharacterLimit function
 
 const textBoxFieldIDs = [48, 49, 52, 53, 76, 86, 56]; // lists paragraph fields
-// Character limit 
+/* Character limit */
 for (let i = 0; i < textBoxFieldIDs.length; i++) {
   showCharacterLimit('view_83','field_'+ textBoxFieldIDs[i],500); // background info page view
   showCharacterLimit('view_155','field_'+ textBoxFieldIDs[i],500); // edit application page view
@@ -100,39 +86,21 @@ $(document).on('knack-scene-render.any', function(event, view) {
   $disabledTriggerButton.removeClass("kn-action-link");
 });
 
-/********************************************/
-/*************** Big Buttons ****************/
-/********************************************/
-//Create Big Button nested in a block
-function bigButton(id, view_id, url, fa_icon, button_label, target_blank = false, is_disabled = false, callback = null) {
-  var disabledClass = is_disabled ? " big-button-disabled'" : "'";
-  var newTab = target_blank ? " target='_blank'" : "" ;
-    $( "<a id='" + id + "' class='big-button-container" + disabledClass + " href='" + url + "'"
-      + newTab + "'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></a>" ).appendTo("#" + view_id);
-  if (callback) callback();
+/***************************************/
+/***** Print Menu Button ************/
+/***************************************/
+function printMenuButton(view_id) {
+  $('#' + view_id + ' .knMenuLink').click(function(e) {
+    window.print();
+  });
 }
-	//>>>HOME TAB BUTTONS
-$(document).on('knack-view-render.view_11', function(event, page) {
-  // create large AVAILABLE SERVICES button on the PORTAL page
-    bigButton('available-services', 'view_11', "https://atd.knack.com/mobility-services#available-services/", "list-ul", "Available Services");
+
+/* Print 3 pages menu view button */
+$(document).on('knack-view-render.view_227', function(event, view, data) {
+  printMenuButton('view_227');
 });
-$(document).on('knack-view-render.view_16', function(event, page) {
-  // create large CUSTOMER PORTAL button on the PORTAL page
-    bigButton('available-services', 'view_16', "https://atd.knack.com/mobility-services#portal/", "child", "Customer Portal");
-});
-$(document).on('knack-view-render.view_34', function(event, page) {
-  // create large REQUIRED DOCUMENTS button on the CHAUFFEUR page
-    bigButton('required-documents-chauffeur', 'view_34', "https://atd.knack.com/mobility-services#chauffeur-permit/required-documents-chauffeur/", "files-o", "Required Documents");
-});
-$(document).on('knack-view-render.view_36', function(event, page) {
-  // create large START APPLICATION button on the CHAUFFEUR page
-    bigButton('start-application', 'view_36', "https://atd.knack.com/mobility-services#application-chauffeur/", "arrow-right", "Start Chauffeur Application");
-});
-$(document).on('knack-view-render.view_41', function(event, page) {
-  // create large START APPLICATION button on the CHAUFFEUR page
-    bigButton('sign-up', 'view_41', "https://atd.knack.com/mobility-services#sign-up", "sign-in", "Sign up or Log In ");
-});
-$(document).on('knack-view-render.view_57', function(event, page) {
-  // create large CUSTOMER PORTAL button on the PORTAL page
-    bigButton('available-services', 'view_57', "https://atd.knack.com/mobility-services#portal/", "arrow-right", "Mobility Services Portal");
+
+/* Print 4 pages menu view button */
+$(document).on('knack-view-render.view_228', function(event, view, data) {
+  printMenuButton('view_228');
 });
