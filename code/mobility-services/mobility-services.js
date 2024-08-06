9769c1d5f454e9fda46c95c9170e217c1dd20a2f
Susanne <susanne.gov@austintexas.gov>
2024-08-06 14:59:28 -0500

    Add SSO button
    Added SSO button for Reviewers

--- code/mobility-services/mobility-services.js
+++ code/mobility-services/mobility-services.js
@@ -1,3 +1,39 @@
+/********************************************/
+/*************** Big Buttons ****************/
+/********************************************/
+function bigButton(id, view_id, url, fa_icon, button_label, target_blank = false, is_disabled = false, callback = null) {
+  var disabledClass = is_disabled ? " big-button-disabled'" : "'";
+  var newTab = target_blank ? " target='_blank'" : "" ;
+    $( "<a id='" + id + "' class='big-button-container" + disabledClass + " href='" + url + "'"
+      + newTab + "'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></a>" ).appendTo("#" + view_id);
+  if (callback) callback();
+}
+	//>>>HOME TAB BUTTONS
+$(document).on('knack-view-render.view_11', function(event, page) {
+  // create large AVAILABLE SERVICES button on the PORTAL page
+    bigButton('available-services', 'view_11', "https://atd.knack.com/mobility-services#available-services/", "list-ul", "Available Services");
+});
+$(document).on('knack-view-render.view_16', function(event, page) {
+  // create large CUSTOMER PORTAL button on the PORTAL page
+    bigButton('available-services', 'view_16', "https://atd.knack.com/mobility-services#portal/", "child", "Customer Portal");
+});
+$(document).on('knack-view-render.view_34', function(event, page) {
+  // create large REQUIRED DOCUMENTS button on the CHAUFFEUR page
+    bigButton('required-documents-chauffeur', 'view_34', "https://atd.knack.com/mobility-services#chauffeur-permit/required-documents-chauffeur/", "files-o", "Required Documents");
+});
+$(document).on('knack-view-render.view_36', function(event, page) {
+  // create large START APPLICATION button on the CHAUFFEUR page
+    bigButton('start-application', 'view_36', "https://atd.knack.com/mobility-services#application-chauffeur/", "arrow-right", "Start Chauffeur Application");
+});
+$(document).on('knack-view-render.view_41', function(event, page) {
+  // create large SIGN UP or Log-In button on the PORTAL page
+    bigButton('sign-up', 'view_41', "https://atd.knack.com/mobility-services#sign-up", "sign-in", "Sign up or Log In ");
+});
+$(document).on('knack-view-render.view_57', function(event, page) {
+  // create large CUSTOMER PORTAL button on the PORTAL page
+    bigButton('available-services', 'view_57', "https://atd.knack.com/mobility-services#portal/", "arrow-right", "Mobility Services Portal");
+});
+
 /***************************************/
 /**** Input Mask number for SSN ********/
 /***************************************/
@@ -103,4 +139,56 @@ $(document).on('knack-view-render.view_227', function(event, view, data) {
 /* Print 4 pages menu view button */
 $(document).on('knack-view-render.view_228', function(event, view, data) {
   printMenuButton('view_228');
+});
+
+/***************************************
+ * Enhance SSO button and hide/show default Knack login form with buttons
+ * @parameter {string} viewId - Knack view id to append button link to
+/***************************************/
+function customizeLoginButton(viewId) {
+  // Hide Knack default SSO button, login form, login title, and any other children
+  $("#" + viewId)
+    .children()
+    .hide();
+
+  var url = Knack.url_base + Knack.scene_hash + "auth/COACD";
+
+  // Create a div for Login buttons
+  var $coacdButton = $("<div/>", {
+    id: "coacd-button-login",
+  });
+  $coacdButton.appendTo("#" + viewId);
+
+  // Append Big SSO Login button and non-SSO Login button
+  bigButton("coacd-big-button", "coacd-button-login", url, "sign-in", "Sign-In")
+
+  $coacdButton.append(
+    "<a class='small-button' href='javascript:void(0)'>" +
+      "<div class='small-button-container'><span><i class='fa fa-lock'></i></span><span> Non-COA Sign-In</span></div></a>"
+  );
+
+  // On non-SSO button click, hide SSO and non-SSO buttons and show Knack Login form
+  var $nonCoacdButton = $(".small-button");
+  $nonCoacdButton.click(function () {
+    $("#" + viewId)
+      .children()
+      .show();
+    $(".small-button-container,.big-button-container").hide();
+    $(".kn-sso-container").hide();
+  });
+}
+
+// Call customizeLoginButton on any view render to customize any login page that renders in app
+$(document).on("knack-view-render.any", function (event, page) {
+  // Find SSO button and existing custom button
+  var $ssoButton = $(".kn-sso-container");
+  var $coacdLoginDiv = $("#coacd-button-login");
+
+  // If SSO button exists on page and there isn't already a custom button
+  if ($ssoButton.length && !$coacdLoginDiv.length) {
+    var $ssoView = $ssoButton.closest("[id^=view_]");
+    var viewId = $ssoView.get(0).id;
+
+    customizeLoginButton(viewId);
+  }
 });
