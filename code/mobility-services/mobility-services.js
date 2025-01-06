41333761a1a1ae71c9b82a2eb22cc618c3c90553
Susanne <susanne.gov@austintexas.gov>
2025-01-06 14:58:23 -0600

    Remove prettier format on big button and updated URL
    -Removed Prettier formatting from big button code in JS
    - Updated URL link for image seal to the one from GitBook instead of austintexas.gov (it's the seal with TPW text below same as previous)
    - Tested seal image on print page and seal shows up as expected
    - Big buttons work as expected

--- code/mobility-services/mobility-services.js
+++ code/mobility-services/mobility-services.js
@@ -1,105 +1,41 @@
 /********************************************/
 /*************** Big Buttons ****************/
 /********************************************/
-function bigButton(
-  id,
-  view_id,
-  url,
-  fa_icon,
-  button_label,
-  target_blank = false,
-  is_disabled = false,
-  callback = null
-) {
+function bigButton(id, view_id, url, fa_icon, button_label, target_blank = false, is_disabled = false, callback = null) {
   var disabledClass = is_disabled ? " big-button-disabled'" : "'";
-  var newTab = target_blank ? " target='_blank'" : "";
-  $(
-    "<a id='" +
-      id +
-      "' class='big-button-container" +
-      disabledClass +
-      " href='" +
-      url +
-      "'" +
-      newTab +
-      "'><span><i class='fa fa-" +
-      fa_icon +
-      "'></i></span><span> " +
-      button_label +
-      "</span></a>"
-  ).appendTo("#" + view_id);
+  var newTab = target_blank ? " target='_blank'" : "" ;
+    $( "<a id='" + id + "' class='big-button-container" + disabledClass + " href='" + url + "'"
+      + newTab + "'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></a>" ).appendTo("#" + view_id);
   if (callback) callback();
 }
-//>>>HOME TAB BUTTONS
-$(document).on("knack-view-render.view_11", function (event, page) {
+	//>>>HOME TAB BUTTONS
+$(document).on('knack-view-render.view_11', function(event, page) {
   // create large AVAILABLE SERVICES button on the PORTAL page
-  bigButton(
-    "available-services",
-    "view_11",
-    "https://atd.knack.com/mobility-services#available-services/",
-    "list-ul",
-    "Available Services"
-  );
+    bigButton('available-services', 'view_11', "https://atd.knack.com/mobility-services#available-services/", "list-ul", "Available Services");
 });
-$(document).on("knack-view-render.view_16", function (event, page) {
+$(document).on('knack-view-render.view_16', function(event, page) {
   // create large CUSTOMER PORTAL button on the PORTAL page
-  bigButton(
-    "available-services",
-    "view_16",
-    "https://atd.knack.com/mobility-services#portal/",
-    "child",
-    "Customer Portal"
-  );
+    bigButton('available-services', 'view_16', "https://atd.knack.com/mobility-services#portal/", "child", "Customer Portal");
 });
-$(document).on("knack-view-render.view_34", function (event, page) {
+$(document).on('knack-view-render.view_34', function(event, page) {
   // create large REQUIRED DOCUMENTS button on the CHAUFFEUR page
-  bigButton(
-    "required-documents-chauffeur",
-    "view_34",
-    "https://atd.knack.com/mobility-services#chauffeur-permit/required-documents-chauffeur/",
-    "files-o",
-    "Required Documents"
-  );
+    bigButton('required-documents-chauffeur', 'view_34', "https://atd.knack.com/mobility-services#chauffeur-permit/required-documents-chauffeur/", "files-o", "Required Documents");
 });
-$(document).on("knack-view-render.view_36", function (event, page) {
+$(document).on('knack-view-render.view_36', function(event, page) {
   // create large START APPLICATION button on the CHAUFFEUR page
-  bigButton(
-    "start-application",
-    "view_36",
-    "https://atd.knack.com/mobility-services#application-chauffeur/",
-    "arrow-right",
-    "Start Chauffeur Application"
-  );
+    bigButton('start-application', 'view_36', "https://atd.knack.com/mobility-services#application-chauffeur/", "arrow-right", "Start Chauffeur Application");
 });
-$(document).on("knack-view-render.view_41", function (event, page) {
+$(document).on('knack-view-render.view_41', function(event, page) {
   // create large SIGN UP or Log-In button on the PORTAL page
-  bigButton(
-    "sign-up",
-    "view_41",
-    "https://atd.knack.com/mobility-services#sign-up",
-    "sign-in",
-    "Sign up or Log In "
-  );
+    bigButton('sign-up', 'view_41', "https://atd.knack.com/mobility-services#sign-up", "sign-in", "Sign up or Log In ");
 });
-$(document).on("knack-view-render.view_57", function (event, page) {
+$(document).on('knack-view-render.view_57', function(event, page) {
   // create large CUSTOMER PORTAL button on the PORTAL page
-  bigButton(
-    "available-services",
-    "view_57",
-    "https://atd.knack.com/mobility-services#portal/",
-    "arrow-right",
-    "Mobility Services Portal"
-  );
+    bigButton('available-services', 'view_57', "https://atd.knack.com/mobility-services#portal/", "arrow-right", "Mobility Services Portal");
 });
-$(document).on("knack-view-render.view_383", function (event, page) {
+$(document).on('knack-view-render.view_383', function(event, page) {
   // create large START APPLICATION button on the Operating Authority page
-  bigButton(
-    "start-application",
-    "view_383",
-    "https://atd.knack.com/mobility-services#application-operating-authority",
-    "arrow-right",
-    "Start Operating Authority Application"
-  );
+    bigButton('start-application', 'view_383', "https://atd.knack.com/mobility-services#application-operating-authority", "arrow-right", "Start Operating Authority Application");
 });
 
 /***************************************/
