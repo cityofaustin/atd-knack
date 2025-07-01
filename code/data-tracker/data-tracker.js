96891719bb28f0323198decdfd8c339571dbc3a8
John Clary <john.clary@austintexas.gov>
2025-07-01 14:23:02 -0400

    Refactor MMC issue webmap URL setter

--- code/data-tracker/data-tracker.js
+++ code/data-tracker/data-tracker.js
@@ -133,9 +133,13 @@ $(document).on("knack-view-render.view_958", function (event, page) {
   }, 1000);
 });
 
-$(document).on("knack-scene-render.scene_428", function (event, page) {
+/**
+ * Set the URL of the embedded AGOL map on the MMC issue/service request details page
+ */
+$(document).on("knack-scene-render.scene_428", function () {
   // update iframe src from detail field
-  var iframe_url = $('a[href*="webappviewer"]').attr("href");
+//   var iframe_url = $('a[href*="webappviewer"]').attr("href");
+  var iframe_url = $('.kn-detail.field_1403 a').attr('href');
   $(".view_1852").hide();
   $("#csr_view").attr("src", iframe_url);
 });
@@ -1133,6 +1137,6 @@ $(document).on("knack-scene-render.scene_1607", function (event, scene) {
 /***************************************************************/
 /*** Disable the ability to Click/Touch outside a Modal Page ***/
 /***************************************************************/
-$(document).on("knack-scene-render.any", function (event, scene) {
-  $(".kn-modal-bg").off("click");
+$(document).on('knack-scene-render.any', function(event, scene) {
+    $('.kn-modal-bg').off('click');
 });
