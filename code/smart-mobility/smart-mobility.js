9c5b8e928da051bfcd38ceaef6ac6717e5aa0324
Karo <100615928+KaroEngstrom@users.noreply.github.com>
2025-11-07 16:11:51 -0600

    Community Services/Smart Mobility Portal Password Generation Updates.js
    Adding custom JavaScript function to generate passwords in the app

--- code/smart-mobility/smart-mobility.js
+++ code/smart-mobility/smart-mobility.js
@@ -1,5 +1,3 @@
-const APPLICATION_ID = "618ad3322d11b4002169a6f9";
-
 /********************************************/
 /******** COACD Single Sign On Login ********/
 /********************************************/
@@ -90,33 +88,57 @@ function bigButton(
 
   if (callback) callback();
 }
-  //>>>HOME TAB BUTTONS
+	//>>>HOME TAB BUTTONS
 $(document).on('knack-view-render.view_16', function(event, page) {
   // create large button on the home page
     bigButton('metrobike-employee-benefit', 'view_16', "https://atd.knack.com/bike-benefit-program#metrobike-employee-benefit/", "bicycle", "MetroBike Employee Benefit");
 });
 
 
-  //>>>LIVING STREETS PROGRAM SELECTIONS BUTTONS draft
+	//>>>LIVING STREETS PROGRAM SELECTIONS BUTTONS draft
 $(document).on('knack-view-render.view_410', function(event, page) {
   // create large button on the home page
     bigButton('living-streets-eoi', 'view_410', "https://atd.knack.com/smart-mobility#living-streets-eoi/", "street-view", "Healthy Streets or Play Streets");
 });
-  //>>>LIVING STREETS PROGRAM SELECTIONS BUTTONS draft
+	//>>>LIVING STREETS PROGRAM SELECTIONS BUTTONS draft
 $(document).on('knack-view-render.view_411', function(event, page) {
   // create large button on the home page
     bigButton('living-streets-login', 'view_411', "https://www.austintexas.gov/department/neighborhood-block-parties", "users", "Block Party");
 });
 
-  //>>>LIVING STREETS GETTING STARTED PAGE draft 1
+	//>>>LIVING STREETS GETTING STARTED PAGE draft 1
 $(document).on('knack-view-render.view_451', function(event, page) {
   // create large button on the home page
     bigButton('living-streets-login', 'view_451', "https://www.austintexas.gov/LivingStreets", "home", "Return to Living Streets Home");
 });
-  //>>>LIVING STREETS GETTING STARTED PAGE draft 2
+	//>>>LIVING STREETS GETTING STARTED PAGE draft 2
 $(document).on('knack-view-render.view_450', function(event, page) {
   // create large button on the home page
     bigButton('living-streets-getting-started', 'view_450', "https://atd.knack.com/smart-mobility#living-streets-eoi-new/", "arrow-right", "Start Expression of Interest");
+});
+
+	//>>>LS SPLASH PAGE - Healthy Streets
+$(document).on('knack-view-render.view_832', function(event, page) {
+  // create large button on the home page
+    bigButton('living-streets-getting-started', 'view_832', "https://atd.knack.com/smart-mobility#living-streets-getting-started/", "arrow-right", "Healthy Streets");
+});
+
+	//>>>LS SPLASH PAGE - Play Streets
+$(document).on('knack-view-render.view_833', function(event, page) {
+  // create large button on the home page
+    bigButton('living-streets-getting-started', 'view_833', "https://atd.knack.com/smart-mobility#living-streets-getting-started//", "arrow-right", "Play Streets");
+});
+
+	//>>>LS SPLASH PAGE - Neighborhood Block Party
+$(document).on('knack-view-render.view_834', function(event, page) {
+  // create large button on the home page
+    bigButton('living-streets-getting-started', 'view_834', "https://atd.knack.com/smart-mobility#block-party-getting-started/", "arrow-right", "Neighborhood Block Party");
+});
+
+	//>>>LS NBP Gettign Started PAGE
+$(document).on('knack-view-render.view_836', function(event, page) {
+  // create large button on the home page
+    bigButton('living-streets-getting-started', 'view_836', "https://atd.knack.com/smart-mobility#nbp-login/", "arrow-right", "Start Block Party Application");
 });
 
 /*******************************************/
@@ -180,12 +202,9 @@ $(document).on("knack-scene-render.scene_284", function () {
   disableBreadCrumbsNonAdmin();
 });
 
-  //*** MANAGE WALLET AND MAKE PAYMENT BUTTONS ***
-$(document).on('knack-view-render.view_804', function(event, page) {
-  // create large button on go back to Portal page
-  bigButton('manage-wallet', 'view_804', "https://austin-tx-austin-tx.uat.cityba.se/welcome", "credit-card", "Manage Wallet");
-});
+  //***MANAGE MAKE PAYMENT BUTTON***
 $(document).on('knack-view-render.view_806', function(event, page) {
+  // create large button on go back to Portal page
   // *** see the getCitybaseButton function for the invocation of the make payment bigButton function
 });
 
@@ -193,113 +212,166 @@ $(document).on('knack-view-render.view_806', function(event, page) {
  * Citybase Integration
  */
 
-function getCitybaseButton(payload, viewId) {
-  fetch(
-    "https://invoice-service-austin-tx.uat.cityba.se/invoices/austin_tx_transportation/street_banner",
-    {
-      method: "POST",
+
+function getCitybaseButton(payload, viewId){
+    fetch('https://invoice-service.prod.cityba.se/invoices/austin_tx_transportation/street_banner', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(payload),
-    },
-  )
-    .then((response) => {
-      console.log("Citybase make payment URL:", response.url)
-      bigButton(
-        "make-payment",
-        viewId,
-        response.url,
-        "arrow-right",
-        "Make Payment",
-      );
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
+      .then((response) => {
+        console.log("Citybase make payment URL:", response.url)
+        bigButton('make-payment', viewId, response.url, "arrow-right", "Review and Pay");
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 
 var knackUserToken = Knack.getUserToken();
 
 var headers = {
-  "X-Knack-Application-Id": APPLICATION_ID,
+  "X-Knack-Application-Id": Knack.application_id,
   "X-Knack-REST-API-KEY": "knack",
   Authorization: knackUserToken,
   "content-type": "application/json",
 };
 
 // After the transactions table loads
-$(document).on("knack-view-render.view_817", function (event, page, view) {
+$(document).on('knack-view-render.view_817', function(event, page, view) {
   $('#view_817').hide()
-  var transactionRecord = view[0];
+  var transactionRecord = view[0]
 
   var payload = {
-    allowed_payment_methods: ["CARD", "BANK"],
-    cancel_url: {
-      url: window.location.href,
-      label: "Cancel",
+    "allowed_payment_methods": [
+      "CARD",
+      "BANK"
+    ],
+    "cancel_url": {
+      "url": window.location.href,
+      "label": "Cancel"
     },
-    return_url: {
-      url: window.location.href,
-      label: "Continue",
-    },
-  };
+    "return_url": {
+      "url": window.location.href,
+      "label": "Continue"
+    }
+  }
 
   // make sure record exists
   if (transactionRecord) {
-    payload["line_items"] = [
-      {
-        description: transactionRecord["field_819"],
-        amount: parseInt(transactionRecord["field_833_raw"] * 100),
-        sub_description: transactionRecord["field_849"],
-        custom_attributes: [
+      payload["line_items"] = [{
+        "description": transactionRecord["field_819"],
+        "amount": parseInt(transactionRecord["field_833_raw"]*100),
+        "sub_description": transactionRecord["field_849"],
+        "custom_attributes": [
           {
-            key: "knack_record_id",
-            value: transactionRecord["id"],
+            "key":"knack_record_id",
+            "value":transactionRecord["id"]
           },
           {
-            key: "invoice_number",
-            value: transactionRecord["field_814"],
+            "key": "invoice_number",
+            "value":transactionRecord["field_814"]
           },
           {
-            key: "fund",
-            value: String(transactionRecord["field_829"]),
+            "key": "fund",
+            "value":String(transactionRecord["field_829"])
           },
           {
-            key: "dept",
-            value: String(transactionRecord["field_830"]),
+            "key": "dept",
+            "value":String(transactionRecord["field_830"])
+          },
+                    {
+            "key": "unit",
+            "value":String(transactionRecord["field_831"])
           },
           {
-            key: "unit",
-            value: String(transactionRecord["field_831"]),
-          },
-          {
-            key: "revenue",
-            value: String(transactionRecord["field_832"]),
-          },
+            "key": "revenue",
+            "value":String(transactionRecord["field_832"])
+          }
         ],
-      },
-    ];
-    payload["custom_attributes"] = [
-      {
-        key: "knack_record_id",
-        value: transactionRecord["id"],
-      },
-      {
-        key: "invoice_number",
-        value: transactionRecord["field_814"],
-      },
-      {
-        "key": "parent_record_id",
-        "value": String(transactionRecord["field_815_raw"][0]["id"]),
-      },
-      {
-        key: "knack_app",
-        value: "SMART_MOBILITY",
-      },
-    ];
-    // uncomment line below for debugging
-    // console.log(JSON.stringify(payload));
-    getCitybaseButton(payload, "view_806");
+      }];
+      payload["custom_attributes"]=[{
+            "key":"knack_record_id",
+            "value":transactionRecord["id"]
+          },
+          {
+            "key": "invoice_number",
+            "value":transactionRecord["field_814"]
+          },
+          {
+            "key": "parent_record_id",
+            "value": String(transactionRecord["field_815_raw"][0]["id"]),
+          },
+          {
+            "key": "knack_app",
+            "value": "SMART_MOBILITY"
+          }
+      ]
+      // uncomment line below for debugging / triaging
+      // console.log(JSON.stringify(payload));
+      getCitybaseButton(payload, "view_806")
+    }
+})
+/*******************************/
+/* Generates a Random Password */
+/*******************************/
+function generatePassword() {
+  const PASSWORD_LENGTH = 20;
+  const LOWER = "abcdefghijklmnopqrstuvwxyz";
+  const UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const NUMBERS = "0123456789";
+  const SPECIAL = "!@#$%&*^"; //  `(` and `)` are not special chars according to Knack
+  const ALL_CHARS = LOWER + UPPER + NUMBERS + SPECIAL;
+  /*
+   * Generates a cryptographically secure random integer between 0 and max (inclusive) using rejection sampling to avoid modulo bias.
+   * Must be between 0 and 255 since this uses Uint8Array with 255 as the max value and excludes integers greater than max
+   */
+  function getRandomInt(max) {
+    let int = null;
+    do {
+      const randomIntArray = new Uint8Array(1);
+      crypto.getRandomValues(randomIntArray);
+      int = randomIntArray[0];
+    } while (int !== null && int > max);
+    return int;
   }
+  // Make sure password contains all required character types
+  function hasAllCharacterTypes(password) {
+    const pwArray = password.split("");
+    const hasLower = pwArray.some((char) => LOWER.includes(char));
+    const hasUpper = pwArray.some((char) => UPPER.includes(char));
+    const hasNumber = pwArray.some((char) => NUMBERS.includes(char));
+    const hasSpecial = pwArray.some((char) => SPECIAL.includes(char));
+    return hasLower && hasUpper && hasNumber && hasSpecial;
+  }
+  // Loop until a valid password is generated
+  let password = "";
+  do {
+    password = "";
+    for (let i = 0; i < PASSWORD_LENGTH; i++) {
+      password += ALL_CHARS[getRandomInt(ALL_CHARS.length - 1)];
+    }
+  } while (!hasAllCharacterTypes(password));
+  return password;
+}
+
+// Handler to target a specific login view to load generated password into the password input box - Account Management Page
+$(document).on("knack-view-render.view_567", function (event, scene) {
+  var pw = generatePassword();
+  $('input[name$="password"]').val(pw);
+  $('input[name$="password_confirmation"]').val(pw);
+});
+// Handler to target a specific login view to load generated password into the password input box - Application Details Page
+$(document).on("knack-view-render.view_486", function (event, scene) {
+  var pw = generatePassword();
+  $('input[name$="password"]').val(pw);
+  $('input[name$="password_confirmation"]').val(pw);
+});
+// Handler to target a specific login view to load generated password into the password input box - Add Internal Account
+$(document).on("knack-view-render.view_114", function (event, scene) {
+  var pw = generatePassword();
+  $('input[name$="password"]').val(pw);
+  $('input[name$="password_confirmation"]').val(pw);
 });
