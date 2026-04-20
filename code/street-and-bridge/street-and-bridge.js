10bab200893bfac181abd65be9501dd410ac0d9c
Chrispin <67915167+ChrispinP@users.noreply.github.com>
2026-04-20 17:21:38 -0500

    APP URL & Big Button Modernization

--- code/street-and-bridge/street-and-bridge.js
+++ code/street-and-bridge/street-and-bridge.js
@@ -1,7 +1,9 @@
-/**
- * Enhance SSO button and hide/show default Knack login form with buttons
- * @parameter {string} viewId - Knack view id to append button link to
- */
+// Setting constant variable to this app URL
+const APP_URL = `https://atd.knack.com/${Knack.app.attributes.slug}`;
+
+/********************************************/
+/******** COACD Single Sign On Login ********/
+/********************************************/
 function customizeLoginButton(viewId) {
   // Hide Knack default SSO button, login form, login title, and any other children
   $("#" + viewId)
@@ -12,7 +14,7 @@ function customizeLoginButton(viewId) {
 
   // Create a div for Login buttons
   var $coacdButton = $("<div/>", {
-    id: "coacd-button-login",
+    id: "coacd-button-login"
   });
   $coacdButton.appendTo("#" + viewId);
 
@@ -49,22 +51,32 @@ $(document).on("knack-view-render.any", function (event, page) {
     customizeLoginButton(viewId);
   }
 });
+
 /********************************************/
 /*************** Big Buttons ****************/
 /********************************************/
-//Create Big Button nested in a block
+// Adds big button HTML directly on View id
 function bigButton(id, view_id, url, fa_icon, button_label, target_blank = false, is_disabled = false, callback = null) {
-  var disabledClass = is_disabled ? " big-button-disabled'" : "'";
-  var newTab = target_blank ? " target='_blank'" : "" ;
-    $( "<a id='" + id + "' class='big-button-container" + disabledClass + " href='" + url + "'"
-      + newTab + "'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></a>" ).appendTo("#" + view_id);
+  const disabledClass = is_disabled ? " big-button-disabled'" : "'";
+  const newTab = target_blank ? " target='_blank'" : "" ;
+  const html = `
+    <a id='${id}' 
+       class='big-button-container${disabledClass}' 
+       href='${url}'${newTab}>
+      <span><i class='fa fa-${fa_icon}'></i></span>
+      <span> ${button_label}</span>
+    </a>
+  `;
+
+  $(`#${view_id}`).append(html);
   if (callback) callback();
 }
-	//>>>HOME TAB BUTTONS
+
+// create large Account Administration button on the Home page
 $(document).on('knack-view-render.view_74', function(event, page) {
-  // create large button on the home page
-    bigButton('contact-directory', 'view_74', "https://atd.knack.com/sbo#account-administration/", "users", "Account Administration");
+    bigButton("contact-directory", "view_74", `${APP_URL}#account-administration/`, "users", "Account Administration");
 });
+
 /*******************************/
 /* Generates a Random Password */
 /*******************************/
