b3424862d421eef543ae5a9f48aa10654f305fc8
Chrispin <67915167+ChrispinP@users.noreply.github.com>
2026-04-24 11:26:46 -0500

    APP URL & Button Modernization

--- code/hr/hr.js
+++ code/hr/hr.js
@@ -1,3 +1,6 @@
+// Setting constant variable to this app URL
+const APP_URL = `https://atd.knack.com/${Knack.app.attributes.slug}`;
+
 /********************************************/
 /******** COACD Single Sign On Login ********/
 /********************************************/
@@ -52,93 +55,108 @@ $(document).on("knack-view-render.any", function (event, page) {
 /********************************************/
 /*************** Big Buttons ****************/
 /********************************************/
-//Create Big Button nested in a block
-function bigButton(id, view_id, url, fa_icon, button_label, is_disabled = false, callback = null) {
-  var disabledClass = is_disabled ? " big-button-disabled'" : "'";
-    $( "<a id='" + id + "' class='big-button-container" + disabledClass + " href='" + url + 
-      "'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></a>" ).appendTo("#" + view_id);
+// Adds big button HTML directly on View id
+function bigButton(id, view_id, url, fa_icon, button_label, target_blank = false, is_disabled = false, callback = null) {
+  const disabledClass = is_disabled ? " big-button-disabled'" : "'";
+  const newTab = target_blank ? " target='_blank'" : "" ;
+  const html = `
+    <a id='${id}' 
+       class='big-button-container${disabledClass}' 
+       href='${url}'${newTab}>
+      <span><i class='fa fa-${fa_icon}'></i></span>
+      <span> ${button_label}</span>
+    </a>
+  `;
 
+  $(`#${view_id}`).append(html);
   if (callback) callback();
 }
 
 // create large Add New Telework Request button on the home page
 $(document).on("knack-view-render.view_924", function(event, page) {
-    bigButton("new-telework", "view_924", "https://atd.knack.com/hr#new-telework-request", "plus-circle", "Add New Telework Request");
+    bigButton("new-telework", "view_924", `${APP_URL}#new-telework-request`, "plus-circle", "Add New Telework Request");
 });
 
 // create large Add New EMPLOYEE button on the Programs page
 $(document).on("knack-view-render.view_929", function(event, page) {
-    bigButton("new-telework", "view_929", "https://atd.knack.com/hr#new-telework-request", "times", "Employees");
+    bigButton("new-employee", "view_929", `${APP_URL}#new-telework-request`, "times", "Employees");
 });
 
 // create large Add New HIRING RESOURCES button on the Programs page
 $(document).on("knack-view-render.view_930", function(event, page) {
-    bigButton("new-telework", "view_930", "https://atd.knack.com/hr#hiring-resources/", "users", "Hiring Resources");
+    bigButton("hiring-resources", "view_930", `${APP_URL}#hiring-resources/`, "users", "Hiring Resources");
 });
 
 // create large Add New STAFF FORMS button on the Programs page
 $(document).on("knack-view-render.view_931", function(event, page) {
-    bigButton("new-telework", "view_931", "https://atd.knack.com/hr#forms/", "files-o", "Staff Forms");
+    bigButton("staff-forms", "view_931", `${APP_URL}#forms/`, "files-o", "Staff Forms");
 });
 
 // create large Employee Logs | District Maintenance button on the home page
 $(document).on("knack-view-render.view_760", function(event, page) {
-    bigButton("employee-logs-district-maintenance", "view_760", "https://atd.knack.com/hr#manage-employee-logs/manage-dm-logs/?view_774_filters=%5B%5D", "cubes", "District Maintenance");
+    bigButton("employee-logs-district-maintenance", "view_760", `${APP_URL}#manage-employee-logs/manage-dm-logs/?view_774_filters=%5B%5D`, "cubes", "District Maintenance");
 });
 // create large Employee Logs | Pavement Operations button on the home page
 $(document).on("knack-view-render.view_761", function(event, page) {
-    bigButton("employee-logs-pavement-operations", "view_761", "https://atd.knack.com/hr#manage-employee-logs/manage-pvm-logs/?view_777_filters=%5B%5D", "road", "Pavement Operations");
+    bigButton("employee-logs-pavement-operations", "view_761", `${APP_URL}#manage-employee-logs/manage-pvm-logs/?view_777_filters=%5B%5D`, "road", "Pavement Operations");
 });
 // create large Employee Logs | District Maintenance button on the home page
 $(document).on("knack-view-render.view_762", function(event, page) {
-    bigButton("employee-logs-utilities-and-structures", "view_762", "https://atd.knack.com/hr#manage-employee-logs/manage-usd-logs/?view_780_filters=%5B%5D", "wrench", "Utilities and Structures");
+    bigButton("employee-logs-utilities-and-structures", "view_762", `${APP_URL}#manage-employee-logs/manage-usd-logs/?view_780_filters=%5B%5D`, "wrench", "Utilities and Structures");
 });
 // create large Employee Logs | Logistics button on the home page
 $(document).on("knack-view-render.view_827", function(event, page) {
-    bigButton("manage-logistic-logs", "view_827", "https://atd.knack.com/hr#manage-employee-logs/manage-logistic-logs/?view_822_filters=%5B%5D", "truck", "Logistics");
+    bigButton("manage-logistic-logs", "view_827", `${APP_URL}#manage-employee-logs/manage-logistic-logs/?view_822_filters=%5B%5D`, "truck", "Logistics");
 });
 // create large Training Buttons | My Training Dashboard
 $(document).on("knack-view-render.view_845", function(event, page) {
-    bigButton("my-training-dashboard", "view_845", "https://atd.knack.com/hr#my-training-dashboard", "tachometer", "My Training");
+    bigButton("my-training-dashboard", "view_845", `${APP_URL}#my-training-dashboard`, "tachometer", "My Training");
 });
 // create large Training Buttons | View Training Classes
 $(document).on("knack-view-render.view_935", function(event, page) {
-    bigButton("view-training-candidates", "view_935", "https://atd.knack.com/hr#view-training-candidates", "users", "View Training Candidates");
+    bigButton("view-training-candidates", "view_935", `${APP_URL}#view-training-candidates`, "users", "View Training Candidates");
 });
 // create large Training Buttons | Manage Training Candidates
 $(document).on("knack-view-render.view_844", function(event, page) {
-    bigButton("manage-training-candidates", "view_844", "https://atd.knack.com/hr#manage-training-candidates/", "users", "Manage Training Candidates");
+    bigButton("manage-training-candidates", "view_844", `${APP_URL}#manage-training-candidates/`, "users", "Manage Training Candidates");
 });
 // create large Training Buttons | Enroll in a Program
 $(document).on("knack-view-render.view_889", function(event, page) {
-    bigButton("program-elgibility", "view_889", "https://atd.knack.com/hr#program-eligibility/", "check-square", "Enroll in a Training Program");
+    bigButton("program-elgibility", "view_889", `${APP_URL}#program-eligibility/`, "check-square", "Enroll in a Training Program");
 });
 // create large buttons - Manage Nominations 
 $(document).on("knack-view-render.view_996", function(event, page) {
-    bigButton("manage-nominations", "view_996", "https://atd.knack.com/hr#manage-nominations/", "trophy", "Manage Committee Nominations");
+    bigButton("manage-nominations", "view_996", `${APP_URL}#manage-nominations/`, "trophy", "Manage Committee Nominations");
 });
 // create large buttons - Self-Nominations 
 $(document).on("knack-view-render.view_997", function(event, page) { 
-  bigButton("view-nominations", "view_997", "https://atd.knack.com/hr#view-nominations/", "star", "View Committee Nominations"); 
+  bigButton("view-nominations", "view_997", `${APP_URL}#view-nominations/`, "star", "View Committee Nominations"); 
 });
 // create large buttons - View Nominations 
 $(document).on("knack-view-render.view_998", function(event, page) { 
-  bigButton("commitee-self-nominations", "view_998", "https://atd.knack.com/hr#committee-self-nomination/", "user", "Committee Self-Nomination"); 
+  bigButton("commitee-self-nominations", "view_998", `${APP_URL}#committee-self-nomination/`, "user", "Committee Self-Nomination"); 
 });
 // create large buttons - DM Nominations
 $(document).on("knack-view-render.view_1031", function(event, page) {
-    bigButton("committee-member-nomination", "view_1031", "https://atd.knack.com/hr#committee-member-nomination/", "users", "Committee DM Nomination");
+    bigButton("committee-member-nomination", "view_1031", `${APP_URL}#committee-member-nomination/`, "users", "Committee DM Nomination");
 });
 
 /********************************************/
 /************** Small Buttons ***************/
 /********************************************/
-//Create Small Button nested in a block
+//Adds Small Button HTML directly on View ID
 function smallButton(id, view_id, url, fa_icon, button_label, is_disabled = false, callback = null) {
-  var disabledClass = is_disabled ? " small-button-disabled'" : "'";
-    $( "<a id='" + id + "' class='back-button" + disabledClass + " href='" + url + 
-      "'><span><i class='fa fa-" + fa_icon + "'></i></span><span> " + button_label + "</span></a>" ).appendTo("#" + view_id);
+  const disabledClass = is_disabled ? " small-button-disabled'" : "'";
+  const html = `
+    <a id='${id}' 
+       class='back-button${disabledClass}' 
+       href='${url}'>
+      <span><i class='fa fa-${fa_icon}'></i></span>
+      <span> ${button_label}</span>
+    </a>
+  `;
 
+  $(`#${view_id}`).append(html);
   if (callback) callback();
 }
 
@@ -160,9 +178,10 @@ $(document).on('knack-scene-render.any', function(event, scene) {
     $('.kn-modal-bg').off('click');
 });
 
-/*** Disable Breadcrumb Navigation Links ***/
-/*******************************************/
-function disableBreadCrumbsNonAdmin() {
+/****************************************************/
+/*** Disable Breadcrumb Navigation Links Function ***/
+/****************************************************/
+function disableBreadcrumbLinks() {
   if (!Knack.user.session) {
     $(".kn-crumbtrail a").each(function () {
       $(this).replaceWith($(this).text());
@@ -170,19 +189,15 @@ function disableBreadCrumbsNonAdmin() {
   }
 }
 
-//Page to disable crumbtrail in SSPR Details
-$(document).on("knack-scene-render.scene_165", function () {
-  disableBreadCrumbsNonAdmin();
-});
+const BREADCRUMB_SCENES = [
+  // Draft Builder
+  'scene_165', // SSPR Details page
+  'scene_471', // Self Nomination page
+  'scene_485', // Member Nomination page
+];
 
-//Page to disable Self Nomination in New Self Nomination
-$(document).on("knack-scene-render.scene_471", function () {
-  disableBreadCrumbsNonAdmin();
-});
-
-//Page to disable Member Nomination in New Member Nomination
-$(document).on("knack-scene-render.scene_485", function () {
-  disableBreadCrumbsNonAdmin();
+BREADCRUMB_SCENES.forEach(scene => {
+  $(document).on(`knack-scene-render.${scene}`, disableBreadcrumbLinks);
 });
 
 /*Hide all Utility page links and hyphens on Employee Sign In page*/
